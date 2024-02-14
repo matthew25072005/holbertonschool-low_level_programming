8f8e0966741b78b71608ae9e53a8bcea646a3074
matthew25072005 <8855@holbertonstudents.com>
2024-02-14 18:47:59 +0000

    xd

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -3,9 +3,9 @@
 int main(void)
 {
 
-    printf("%s%s%s\n", "with", " proper grammar, but the outcome is a piece of art", ",");
+	printf("with proper grammar, but the outcome is a piece of art,");
 
-    return (0);
+	return (0);
 }
 
 
