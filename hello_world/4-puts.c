104b62544a33c76584e4a2dc74db6b2f7183693a
matthew25072005 <8855@holbertonstudents.com>
2024-02-14 18:07:52 +0000

    xd

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,2 +1,12 @@
-.
+#include <stdio.h>
 
+int main() {
+    // Using puts to print the desired string
+    puts("Programming is like building a multilingual puzzle");
+
+    // Printing a new line
+    puts("");
+
+    // Ending the program with the value 0
+    return 0;
+}
