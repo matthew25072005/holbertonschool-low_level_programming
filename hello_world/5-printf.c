28ac1a423e9c50a21aa661627140cbfd8964de03
matthew25072005 <8855@holbertonstudents.com>
2024-02-14 18:56:58 +0000

    xd

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
-
+/**
+*main - que caca c
+*Return: c it's a fa***** sh**
+*/
 int main(void)
 {
 
-	printf("%s%s%s\n", "with", " proper grammar, but the outcome is a piece of art", ",");
+	printf("%s\n", "with", " proper grammar, but the outcome is a piece of art,");
 
 	return (0);
 }
