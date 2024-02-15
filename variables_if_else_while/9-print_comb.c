ce302962cc1bb315515141792dc95748663eefec
matthew25072005 <8855@holbertonstudents.com>
2024-02-15 17:40:22 +0000

    xd

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	putchar('0' + 0);
+	putchar(',');
+	putchar(' ');
+	putchar('0' + 1);
+	putchar(',');
+	putchar(' ');
+	putchar('0' + 2);
+	putchar(',');
+	putchar(' ');
+	putchar('0' + 3);
+	putchar(',');
+	putchar(' ');
+	putchar('0' + 4);
+	putchar(',');
+	putchar(' ');
+	putchar('0' + 5);
+	putchar(',');
+	putchar(' ');
+	putchar('0' + 6);
+	putchar(',');
+	putchar(' ');
+	putchar('0' + 7);
+	putchar(',');
+	putchar(' ');
+	putchar('0' + 8);
+	putchar(',');
+	putchar(' ');
+	putchar('0' + 9);
+	putchar('\n');
+
+	return (0);
+}
