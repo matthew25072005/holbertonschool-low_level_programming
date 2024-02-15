8027f7e9cf2d9aa0d64e35e32d936b047090a2fc
matthew25072005 <8855@holbertonstudents.com>
2024-02-15 16:48:45 +0000

    xd

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -7,17 +7,7 @@
 
 int main(void)
 {
-	putchar('0' + 0);
-	putchar('0' + 1);
-	putchar('0' + 2);
-	putchar('0' + 3);
-	putchar('0' + 4);
-	putchar('0' + 5);
-	putchar('0' + 6);
-	putchar('0' + 7);
-	putchar('0' + 8);
-	putchar('0' + 9);
-
+	putchar('0' + 0 * 1));
 	putchar('\n');
 
 	return (0);
