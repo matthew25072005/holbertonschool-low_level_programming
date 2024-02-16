7bd457687e79f5fac4113b4880bd1050bc2bb7de
matthew25072005 <8855@holbertonstudents.com>
2024-02-16 13:44:07 +0000

    xd

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,13 +1,10 @@
-/*
- * File: 2-print_alphabet_x10.c
- * Auth: Brennan D Baraban
- */
-
-#include "holberton.h"
+#include <stdio.h>
 
 /**
- * print_alphabet_x10 - Prints 10 times the alphabet, in lowercase.
- */
+*print_alphabet_x10 - penis
+*return: qsy
+*/
+
 void print_alphabet_x10(void)
 {
 	int count = 0;
@@ -16,7 +13,7 @@ void print_alphabet_x10(void)
 	while (count++ <= 9)
 	{
 		for (letter = 'a'; letter <= 'z'; letter++)
-			_putchar(letter);
-		_putchar('\n');
+			putchar(letter);
+		putchar('\n');
 	}
 }
