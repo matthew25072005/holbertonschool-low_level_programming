2cdb8be7cd4b9fb196c182162817fd05b85681a3
matthew25072005 <8855@holbertonstudents.com>
2024-02-16 16:22:10 +0000

    xd

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -9,12 +9,13 @@ int _islower(int c)
 {
 	if (islower(c))
 	{
-	if (c >= 'a' && c <= 'z')
-	{
-	return (1);
+		if (c >= 'a' && c <= 'z')
+		{
+		return (1);
+		}
+		else
+		{
+		return (0);
+		}
 	}
-	else
-	{
-	return (0);
-	}
-
+}
