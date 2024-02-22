0ad4472fb481c577290c0024727b544add26ea03
matthew25072005 <8855@holbertonstudents@gmail.com>
2024-02-22 18:57:53 +0000

    xd

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -11,7 +11,7 @@ void rev_string(char *s)
 	int a = 0, b;
 
 	char letra;
-	while (s[a] != '\0')
+	while (s[a] != '\0')	
 		a++;
 	a--;
 
@@ -20,5 +20,5 @@ void rev_string(char *s)
 		letra = s[b];
 		s[b] = s[a - b];
 		s[a - b] = letra;
-			}
+	}
 }
