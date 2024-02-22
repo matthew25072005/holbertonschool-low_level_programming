20ffbb0a9909d812232258bf137c04b9b8fd6f3f
matthew25072005 <8855@holbertonstudents@gmail.com>
2024-02-22 13:36:04 +0000

    xd

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -6,12 +6,14 @@
  */
 void print_rev(char *s)
 {
-	int len = 0, index;
+	int index = 0;
 
-	while (s[index++] != '\0')
-		len++;
+	while (s[index] != '\0')
+		index++;
 
-	for (index = len - 1; index >= 0; index--)
+	index--;
+
+	for (; index >= 0; index--)
 		_putchar(s[index]);
 
 	_putchar('\n');
