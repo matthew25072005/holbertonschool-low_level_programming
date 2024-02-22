e18c211afaf4b06c9ebbede64a5338149516dd8c
matthew25072005 <8855@holbertonstudents@gmail.com>
2024-02-22 18:53:47 +0000

    xd

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -1,17 +1,17 @@
 #include "main.h"
 
 /**
-  * rev_string - Reverses a string
-  * @s: The string to be modified
-  *
-  * Return: void
-  */
+* rev_string - Reverses a string
+* @s: The string to be modified
+*
+* Return: void
+*/
 void rev_string(char *s)
 {
 	int a = 0, b;
 
 	char letra;
-	while (s[a] != '\0')
+	while(s[a] != '\0')
 		a++;
 	a--;
 
