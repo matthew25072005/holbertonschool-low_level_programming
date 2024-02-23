85245630200ab20e9b5f0e14392a9d9397e7bd8b
matthew25072005 <8855@holbertonstudents.com>
2024-02-23 23:56:50 +0000

    xd

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+*puts_half - sii
+*@s: The string to be modified
+*/
+
+void puts_half(char *s)
+{
+	int a = 5, b;
+
+	while (s[a] != '\0')
+		a++;
+	a--;
+
+	for (b = 5; b <= a; b++)
+		_putchar(s[b]);
+
+	_putchar('\n');
+}
