01b607d88ddb7b7db090d2a96ce3618c3f9b7084
matthew25072005 <8855@holbertonstudents.com>
2024-02-26 14:40:52 +0000

    xd

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -1,6 +1,14 @@
-
 #include "main.h"
 
+/**
+ * _strncat - Concatenates two strings using at most
+ *            an inputted number of bytes from src.
+ * @dest: The string to be appended upon.
+ * @src: The string to be appended to dest.
+ * @n: The number of bytes from src to be appended to dest.
+ *
+ * Return: A pointer to the resulting string dest.
+ */
 
 char *_strncat(char *dest, char *src, int n)
 {
@@ -9,8 +17,8 @@ char *_strncat(char *dest, char *src, int n)
 	while (dest[a++])
 		b++;
 
-	for (a = 0;src[a] && a < n; a++)
-		dest[b++] = src [a];
+	for (a = 0; src[a] && a < n; a++)
+		dest[b++] = src[a];
 
 	return (dest);
 }
