e593570988529de84d84c3f5770cee213b2e1359
matthew25072005 <8855@holbertonstudents.com>
2024-03-05 13:39:27 +0000

    xd

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,6 +1,13 @@
 #ifndef dog_h
 #define dog_h
 
+/**
+ * struct dog - A new type describing a dog.
+ * @name: The name of the dog.
+ * @age: The age of the dog.
+ * @owner: The owner of the dog.
+ */
+
 struct dog
 {
 	char *name;
