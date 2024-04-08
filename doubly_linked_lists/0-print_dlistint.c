a1888e5b8e6b6a508e3cf5b2afc032eda158c916
matthew25072005 <8855@holbertonstudents.com>
2024-04-08 22:24:01 +0000

    xd

---
+++ doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct dlistint_s
+{
+	int n;
+	struct dlistint_s *prev;
+	struct dlistint_s *next;
+} dlistint_t;
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	const dlistint_t *current;
+	size_t count = 0;
+
+	current = h;
+
+	while (current != NULL)
+	{
+		printf("%d\n", current->n);
+		current = current->next;
+		count++;
+	}
+
+	return (count);
+}
+
